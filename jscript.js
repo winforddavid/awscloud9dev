81baebb00c6b746c1676b1e88588839790d7af84
EC2 Default User <ec2-user@ip-172-31-34-254.eu-west-2.compute.internal>
2021-10-07 21:13:33 +0000

    All files edited to work

---
+++ jscript.js
@@ -0,0 +1,12 @@
+let tmpName1 = "Winford ";
+let tmpName2 = "David";
+let profession = prompt("Please enter your occupation title?");
+// let fullname = ("Hi my name is " + tmpName1 + tmpName2);
+// document.write(fullname);
+let objFullname = {
+        oFName: tmpName1,
+        oSName: tmpName2,
+        oTitle: profession,
+}
+//document.write("Hi My name is " + objFullname.oFName, objFullname.oSName + "My Profession is " objFullname.oTitle);
+document.write("My Name is " + objFullname.oFName + objFullname.oSName + "," + "My Profession is " + objFullname.oTitle);
